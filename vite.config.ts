d8c05056c525e43c7abf0ee1cc09ac276aab2fb3
Obed Burindi <obedburindi@gmail.com>
2025-12-03 11:27:30 +0200

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import viteCompression from 'vite-plugin-compression';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Mwinda-Grand-Lac/',
+      base: '/Web_Mwinda-Grand-Lac/',
       server: {
         port: 3000,
         host: '0.0.0.0',
